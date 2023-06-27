994ff407eacdbc4a53d7ea3eefae8970f1bf7f59
AnetRo <beatrice.ccq@gmail.com>
2023-06-28 00:30:37 +0300

    route App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,31 +7,6 @@ import { MovieDetails } from './movies/MovieDetails';
 import { Cast } from './movies/Cast';
 import { Reviews } from './movies/Reviews';
 
-// export const App = () => {
-//   return (
-//     <Router>
-//       <nav>
-//         <ul>
-//           <li>
-//             <NavLink exact to="/" activeClassName="active">Home</NavLink>
-//           </li>
-//           <li>
-//             <NavLink to="/movies" activeClassName="active">Movies</NavLink>
-//           </li>
-//         </ul>
-//       </nav>
-
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/movies/*" element={<Movies />} />
-//         <Route path="/movies/:movieId" element={<MovieDetails />} />
-//         <Route path="/movies/:movieId/cast" element={<Cast />} />
-//         <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
 
 export const App = () => {
   return (
@@ -61,7 +36,30 @@ export const App = () => {
 
 
 
+// export const App = () => {
+//   return (
+//     <Router>
+//       <nav>
+//         <ul>
+//           <li>
+//             <NavLink exact to="/" activeClassName="active">Home</NavLink>
+//           </li>
+//           <li>
+//             <NavLink to="/movies" activeClassName="active">Movies</NavLink>
+//           </li>
+//         </ul>
+//       </nav>
 
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/movies/*" element={<Movies />} />
+//         <Route path="/movies/:movieId" element={<MovieDetails />} />
+//         <Route path="/movies/:movieId/cast" element={<Cast />} />
+//         <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
 
 
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,33 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // Викликати API TMDb для отримання списку фільмів
+    axios.get('https://api.themoviedb.org/3/trending/movie/week?api_key=889c6c698b758e37f5a9c5b22a486a89')
+      .then(response => {
+        setMovies(response.data.results);
+      })
+      .catch(error => {
+        console.error('Помилка при отриманні списку фільмів:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to the home page!</p>
+      {movies.map(movie => (
+        <div key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+// export const Home = () => {
+//     const [movies, setMovies] = useState([]);
   
-    useEffect(() => {
-      // Викликати API TMDb для отримання списку фільмів
-      axios.get('https://api.themoviedb.org/3/trending/movie/week?api_key=889c6c698b758e37f5a9c5b22a486a89')
-        .then(response => {
-          setMovies(response.data.results);
-        })
-        .catch(error => {
-          console.error('Помилка при отриманні списку фільмів:', error);
-        });
-    }, []);
+//     useEffect(() => {
+//       // Викликати API TMDb для отримання списку фільмів
+//       axios.get('https://api.themoviedb.org/3/trending/movie/week?api_key=889c6c698b758e37f5a9c5b22a486a89')
+//         .then(response => {
+//           setMovies(response.data.results);
+//         })
+//         .catch(error => {
+//           console.error('Помилка при отриманні списку фільмів:', error);
+//         });
+//     }, []);
   
-    return (
-      <div>
-        <h2>Home</h2>
-        {movies.map(movie => (
-          <div key={movie.id}>
-            <h3>{movie.title}</h3>
-            <p>Overview: {movie.overview}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
+//     return (
+//       <div>
+//         <h2>Home</h2>
+//         {movies.map(movie => (
+//           <div key={movie.id}>
+//             <h3>{movie.title}</h3>
+//             <p>Overview: {movie.overview}</p>
+//           </div>
+//         ))}
+//       </div>
+//     );
+//   }
 
 
 // export const Home = () => {
