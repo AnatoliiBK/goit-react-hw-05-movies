2646df553bb065c7f1a6c598b017e80e55d66be0
AnetRo <beatrice.ccq@gmail.com>
2023-07-01 02:03:53 +0300

    Update Movies.jsx

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -33,10 +33,16 @@ export const Movies = () => {
       });
   };
 
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
     <div>
       <h2>Movies</h2>
-      <input type="text" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
+      <input type="text" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} onKeyPress={handleKeyPress} />
       <button onClick={handleSearch}>Search</button>
 
       {movies.length > 0 ? (
@@ -59,6 +65,66 @@ export const Movies = () => {
     </div>
   );
 };
+
+
+
+// export const Movies = () => {
+//   const [searchQuery, setSearchQuery] = useState('');
+//   const [movies, setMovies] = useState([]);
+//   const location = useLocation();
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     // Перевірка, чи є наявний список фільмів у стані локації
+//     if (location.state && location.state.movies) {
+//       setMovies(location.state.movies);
+//     }
+//   }, [location.state]);
+
+//   const handleSearch = () => {
+//     if (searchQuery.trim() === '') {
+//       return;
+//     }
+
+//     // Викликати API TMDb для пошуку фільмів
+//     axios
+//       .get(`https://api.themoviedb.org/3/search/movie?api_key=889c6c698b758e37f5a9c5b22a486a89&query=${searchQuery}`)
+//       .then(response => {
+//         setMovies(response.data.results);
+//         // Зберігати список фільмів у стані локації
+//         navigate('.', { state: { movies: response.data.results } });
+//       })
+//       .catch(error => {
+//         console.error('Помилка при отриманні списку фільмів:', error);
+//       });
+//   };
+
+//   return (
+//     <div>
+//       <h2>Movies</h2>
+//       <input type="text" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
+//       <button onClick={handleSearch}>Search</button>
+
+//       {movies.length > 0 ? (
+//         <div>
+//           <h3>Search results:</h3>
+//           {movies.map(movie => (
+//             <ul key={movie.id}>
+//               <li>
+//                 <Link to={{ pathname: `/movies/${movie.id}`, state: { movies: movies } }}>{movie.title}</Link>
+//               </li>
+//             </ul>
+//           ))}
+//         </div>
+//       ) : (
+//         <p>
+//           Enter a movie title to search or{' '}
+//           <Link to="/">go to the Home page to select a movie</Link>
+//         </p>
+//       )}
+//     </div>
+//   );
+// };
 
 
 
